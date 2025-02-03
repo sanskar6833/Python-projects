ceb49a427513255ef429f99fcf1e88eed7848891
sanskar683 <sanskar683@gmail.com>
2025-02-03 21:07:06 +0530

    initial commit

---
+++ .history/Password_generator_20250203204546.py
@@ -0,0 +1,4 @@
+import random
+import string
+char= string.ascii_letters + string.digits + string.punctuation
+print(char)
---
+++ Password_generator.py
@@ -0,0 +1,8 @@
+import random
+import string
+char= string.ascii_letters + string.digits + string.punctuation
+n= int(input("enter desired length of password"))
+password=""
+for i in range (n):
+    password+=random.choice(char)
+print(f"your password = {password}")    
